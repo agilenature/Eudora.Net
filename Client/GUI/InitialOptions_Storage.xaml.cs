cfb844408e8946e838ccd400fbcd2080f47d6a45
ispeedonthe405 <ispeedonthe405@gmail.com>
2024-08-27 22:29:42 -0700

    Namespace cleanup

--- Client/GUI/InitialOptions_Storage.xaml.cs
+++ Client/GUI/InitialOptions_Storage.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Eudora.Net.GUI
 {
@@ -32,6 +20,7 @@ namespace Eudora.Net.GUI
             ofd.Multiselect = false;
             ofd.Title = "Select the folder to serve as the data store";
             ofd.ValidateNames = true;
+            
             if (ofd.ShowDialog() == true)
             {
                 tb_Datastore.Text = ofd.FolderName;
